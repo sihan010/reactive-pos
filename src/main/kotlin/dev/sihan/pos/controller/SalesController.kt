0773df984b8d93c4e2bdc85c3b09a6240f6646c5
Sihan Mo <sihan.thegreat@gmail.com>
2023-02-18 20:03:20 +0900

    made the api fully reactive

--- src/main/kotlin/dev/sihan/pos/controller/SalesController.kt
+++ src/main/kotlin/dev/sihan/pos/controller/SalesController.kt
@@ -9,17 +9,18 @@ import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Controller
 class SalesController(private val salesService: SalesService) {
 
     @MutationMapping("MakePayment")
-    suspend fun makePayment(@Argument payment: PaymentIn): PaymentOut {
+    fun makePayment(@Argument payment: PaymentIn): Mono<PaymentOut> {
         return salesService.addSale(payment)
     }
 
     @QueryMapping("GetSalesHistory")
-    suspend fun getSalesHistory(@Argument startDateTime: String, @Argument endDateTime: String): Flux<SalesHistoryOut> {
+    fun getSalesHistory(@Argument startDateTime: String, @Argument endDateTime: String): Flux<SalesHistoryOut> {
         return salesService.getSalesHistory(startDateTime, endDateTime)
     }
 }
